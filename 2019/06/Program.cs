4264f20cea3c2436578a410bda595e6477278c68
keydon <keydon@gmail.com>
2019-12-06 19:43:58 +0100

    --- Day 6: Universal Orbit Map ---
    You've landed at the Universal Orbit Map facility on Mercury. Because navigation in space often involves transferring between orbits, the orbit maps here are useful for finding efficient routes between, for example, you and Santa. You download a map of the local orbits (your puzzle input).
    
    Except for the universal Center of Mass (COM), every object in space is in orbit around exactly one other object. An orbit looks roughly like this:
    
                      \
                       \
                        |
                        |
    AAA--> o            o <--BBB
                        |
                        |
                       /
                      /
    In this diagram, the object BBB is in orbit around AAA. The path that BBB takes around AAA (drawn with lines) is only partly shown. In the map data, this orbital relationship is written AAA)BBB, which means "BBB is in orbit around AAA".
    
    Before you use your map data to plot a course, you need to make sure it wasn't corrupted during the download. To verify maps, the Universal Orbit Map facility uses orbit count checksums - the total number of direct orbits (like the one shown above) and indirect orbits.
    
    Whenever A orbits B and B orbits C, then A indirectly orbits C. This chain can be any number of objects long: if A orbits B, B orbits C, and C orbits D, then A indirectly orbits D.
    
    For example, suppose you have the following map:
    
    COM)B
    B)C
    C)D
    D)E
    E)F
    B)G
    G)H
    D)I
    E)J
    J)K
    K)L
    Visually, the above map of orbits looks like this:
    
            G - H       J - K - L
           /           /
    COM - B - C - D - E - F
                   \
                    I
    In this visual representation, when two objects are connected by a line, the one on the right directly orbits the one on the left.
    
    Here, we can count the total number of orbits as follows:
    
    D directly orbits C and indirectly orbits B and COM, a total of 3 orbits.
    L directly orbits K and indirectly orbits J, E, D, C, B, and COM, a total of 7 orbits.
    COM orbits nothing.
    The total number of direct and indirect orbits in this example is 42.
    
    What is the total number of direct and indirect orbits in your map data?
    
    Your puzzle answer was 144909.
    
    --- Part Two ---
    Now, you just need to figure out how many orbital transfers you (YOU) need to take to get to Santa (SAN).
    
    You start at the object YOU are orbiting; your destination is the object SAN is orbiting. An orbital transfer lets you move from any object to an object orbiting or orbited by that object.
    
    For example, suppose you have the following map:
    
    COM)B
    B)C
    C)D
    D)E
    E)F
    B)G
    G)H
    D)I
    E)J
    J)K
    K)L
    K)YOU
    I)SAN
    Visually, the above map of orbits looks like this:
    
                              YOU
                             /
            G - H       J - K - L
           /           /
    COM - B - C - D - E - F
                   \
                    I - SAN
    In this example, YOU are in orbit around K, and SAN is in orbit around I. To move from K to I, a minimum of 4 orbital transfers are required:
    
    K to J
    J to E
    E to D
    D to I
    Afterward, the map of orbits looks like this:
    
            G - H       J - K - L
           /           /
    COM - B - C - D - E - F
                   \
                    I - SAN
                     \
                      YOU
    What is the minimum number of orbital transfers required to move from the object YOU are orbiting to the object SAN is orbiting? (Between the objects they are orbiting - not between YOU and SAN.)
    
    Your puzzle answer was 259.

---
+++ 2019/06/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+namespace day02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==== Part 1 ====");
+            var stopwatch = Stopwatch.StartNew();
+            var orbitMap = File
+                .ReadAllLines("input.txt")
+                .Select(l => {
+                    var orbs = l.Splizz(")").ToArray();
+                    return new {Inner = orbs[0], Outter = orbs[1]};
+                })
+                .Aggregate(new OrbitMap(), (map, orb) => map.AddOrbit(orb.Inner, orb.Outter));
+            
+            stopwatch.Stop();
+            Console.WriteLine("Orbitmap checksum: {0}", orbitMap.GetCecksum());
+            Console.WriteLine("Calculation took: {0}", stopwatch.Elapsed);
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
+            Console.WriteLine("==== Part 2 ====");
+            stopwatch.Start();
+            var you = orbitMap.GetPlanet("YOU");
+            var san = orbitMap.GetPlanet("SAN");
+
+            var yourPlants = you.GetInnerPlanets().ToList();
+            var sansPlanets = san.GetInnerPlanets().ToList();
+            var intersect = yourPlants.Intersect(sansPlanets);
+            var union = yourPlants.Union(sansPlanets);
+
+            var transfers = union.Except(intersect).Count();
+
+            stopwatch.Stop();
+            Console.WriteLine("Amount of transfers from YOU to SAN: {0}", transfers);
+            Console.WriteLine("Calculation took: {0}", stopwatch.Elapsed);
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+
+    internal class Planet
+    {
+        public Planet(string name, Planet isOrbitingAround = null)
+        {
+            Name = name;
+            IsOrbitingAround = isOrbitingAround;
+        }
+        public string Name { get; set; }
+        public Planet IsOrbitingAround { get; set; }
+
+        public IEnumerable<Planet> GetInnerPlanets()
+        {
+            if (IsOrbitingAround == null)
+                yield break;
+
+            yield return IsOrbitingAround;
+            foreach (var inner in IsOrbitingAround.GetInnerPlanets())
+            {
+                yield return inner;
+            }
+        }
+
+        public int OrbitCount => IsOrbitingAround?.OrbitCount + 1 ?? 0;
+        
+        protected bool Equals(Planet other)
+        {
+            return string.Equals(Name, other.Name);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Planet) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (Name != null ? Name.GetHashCode() : 0);
+        }
+    }
+
+    internal class OrbitMap
+    {
+        private readonly Dictionary<string, Planet> planetsDict = new Dictionary<string, Planet>();
+
+        public OrbitMap()
+        {
+        }
+
+        internal OrbitMap AddOrbit(string inner, string outter)
+        {
+            if(!planetsDict.TryGetValue(inner, out var innerPlanet))
+            {
+                innerPlanet = new Planet(inner);
+                planetsDict.Add(inner, innerPlanet);
+            }
+
+            if(!planetsDict.TryGetValue(outter, out var outerPlanet))
+            {
+                outerPlanet = new Planet(outter, innerPlanet);
+                planetsDict.Add(outter, outerPlanet);
+            }
+            else
+            {
+                if(outerPlanet.IsOrbitingAround != null)
+                    throw new Exception("outter already exists");
+                outerPlanet.IsOrbitingAround = innerPlanet;
+            }
+
+            return this;
+        }
+
+        public Planet GetPlanet(string name)
+        {
+            return planetsDict[name];
+        }
+
+        public int GetCecksum()
+        {
+            return planetsDict
+                .Select(p => p.Value.OrbitCount)
+                .Sum();
+        }
+    }
+
+}
+
+public static class Extensions
+{
+    public static IEnumerable<string> Splizz(this string str, params string[] seps){
+        if(seps == null || seps.Length == 0)
+            seps = new[]{";", ","};
+        return str.Split(seps,StringSplitOptions.RemoveEmptyEntries).Select(e => e.Trim());
+    }
+    public static IEnumerable<IEnumerable<string>> Splizz(this IEnumerable<string> enumerable, params string[] seps){
+        foreach (var item in enumerable)
+        {
+            yield return item.Splizz(seps);
+        }
+    }
+}
+
+
