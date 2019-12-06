385a1f6aec48b177179104b0b716104946945981
keydon <keydon@gmail.com>
2019-12-06 01:20:17 +0100

    --- Day 4: Secure Container ---
    You arrive at the Venus fuel depot only to discover it's protected by a password. The Elves had written the password on a sticky note, but someone threw it out.
    
    However, they do remember a few key facts about the password:
    
    It is a six-digit number.
    The value is within the range given in your puzzle input.
    Two adjacent digits are the same (like 22 in 122345).
    Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
    Other than the range rule, the following are true:
    
    111111 meets these criteria (double 11, never decreases).
    223450 does not meet these criteria (decreasing pair of digits 50).
    123789 does not meet these criteria (no double).
    How many different passwords within the range given in your puzzle input meet these criteria?
    
    Your puzzle answer was 2814.
    
    --- Part Two ---
    An Elf just remembered one more important detail: the two adjacent matching digits are not part of a larger group of matching digits.
    
    Given this additional criterion, but still ignoring the range rule, the following are now true:
    
    112233 meets these criteria because the digits never decrease and all repeated digits are exactly two digits long.
    123444 no longer meets the criteria (the repeated 44 is part of a larger group of 444).
    111122 meets the criteria (even though 1 is repeated more than twice, it still contains a double 22).
    How many different passwords within the range given in your puzzle input meet all of the criteria?
    
    Your puzzle answer was 1991.

---
+++ 2019/04/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace day04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==== Part 1 ====");
+            var stopwatch = Stopwatch.StartNew();
+      
+            var possiblePasswords = Enumerable.Range(109165,  576723-109165)
+                    .AsParallel()
+                    .Where(isNotDecreasing)
+                    .Where(hasSiblings)
+                    .ToList();
+
+            Console.WriteLine(">> Possible Passwords: {0} <<", possiblePasswords.Count);
+            stopwatch.Stop();
+            Console.WriteLine("Calculation took : {0}", stopwatch.Elapsed);
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
+            
+            Console.WriteLine("==== Part 2 ====");
+            stopwatch.Start();  
+
+            var possiblePasswordsPart2 = possiblePasswords
+                    .AsParallel()
+                    .Where(hasPairs)
+                    .ToList();  
+                
+            Console.WriteLine(">> Possible Passwords: {0} <<", possiblePasswordsPart2.Count);
+            stopwatch.Stop();
+            Console.WriteLine("Calculation took : {0}", stopwatch.Elapsed);
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        private static bool hasSiblings(int arg)
+        {
+            var str = arg.ToString();
+            for (int i = 0; i < str.Length-1; i++)
+            {
+                var a = str[i];
+                var b = str[i+1];
+
+                if(b == a) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static bool hasPairs(int arg)
+        {
+            var str = arg.ToString();
+            for (int i = 0; i < str.Length-1; i++)
+            {
+                var a = str[i];
+                var b = str[i+1];
+
+                if(b == a) {
+                    var preA = i>0 ? str[i-1] : 'x';
+                    var afterB = i+1 < str.Length-1 ? str[i+2] : 'x';
+                    if(a != preA && b != afterB){
+                        return true;
+                    }
+                }
+                    
+            }
+            return false;
+        }
+
+        private static bool isNotDecreasing(int arg)
+        {
+            var str = arg.ToString();
+            for (int i = 0; i < str.Length-1; i++)
+            {
+                var a = str[i];
+                var b = str[i+1];
+
+                if(b < a)
+                    return false;
+            }
+            return true;
+        }
+    }
+
+    public static class Extensions
+    {
+        public static IEnumerable<string> Splizz(this string str, params string[] seps){
+            if(seps == null || seps.Length == 0)
+                seps = new[]{";", ","};
+            return str.Split(seps,StringSplitOptions.RemoveEmptyEntries).Select(e => e.Trim());
+        }
+    }
+}
