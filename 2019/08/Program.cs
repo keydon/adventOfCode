791c15b88056e37d9dcb68da4f800813da6785b8
keydon <keydon@gmail.com>
2019-12-08 15:26:45 +0100

    --- Day 8: Space Image Format ---
    The Elves' spirits are lifted when they realize you have an opportunity to reboot one of their Mars rovers, and so they are curious if you would spend a brief sojourn on Mars. You land your ship near the rover.
    
    When you reach the rover, you discover that it's already in the process of rebooting! It's just waiting for someone to enter a BIOS password. The Elf responsible for the rover takes a picture of the password (your puzzle input) and sends it to you via the Digital Sending Network.
    
    Unfortunately, images sent via the Digital Sending Network aren't encoded with any normal encoding; instead, they're encoded in a special Space Image Format. None of the Elves seem to remember why this is the case. They send you the instructions to decode it.
    
    Images are sent as a series of digits that each represent the color of a single pixel. The digits fill each row of the image left-to-right, then move downward to the next row, filling rows top-to-bottom until every pixel of the image is filled.
    
    Each image actually consists of a series of identically-sized layers that are filled in this way. So, the first digit corresponds to the top-left pixel of the first layer, the second digit corresponds to the pixel to the right of that on the same layer, and so on until the last digit, which corresponds to the bottom-right pixel of the last layer.
    
    For example, given an image 3 pixels wide and 2 pixels tall, the image data 123456789012 corresponds to the following image layers:
    
    Layer 1: 123
             456
    
    Layer 2: 789
             012
    The image you received is 25 pixels wide and 6 pixels tall.
    
    To make sure the image wasn't corrupted during transmission, the Elves would like you to find the layer that contains the fewest 0 digits. On that layer, what is the number of 1 digits multiplied by the number of 2 digits?
    
    Your puzzle answer was 1072.
    
    --- Part Two ---
    Now you're ready to decode the image. The image is rendered by stacking the layers and aligning the pixels with the same positions in each layer. The digits indicate the color of the corresponding pixel: 0 is black, 1 is white, and 2 is transparent.
    
    The layers are rendered with the first layer in front and the last layer in back. So, if a given position has a transparent pixel in the first and second layers, a black pixel in the third layer, and a white pixel in the fourth layer, the final image would have a black pixel at that position.
    
    For example, given an image 2 pixels wide and 2 pixels tall, the image data 0222112222120000 corresponds to the following image layers:
    
    Layer 1: 02
             22
    
    Layer 2: 11
             22
    
    Layer 3: 22
             12
    
    Layer 4: 00
             00
    Then, the full image can be found by determining the top visible pixel in each position:
    
    The top-left pixel is black because the top layer is 0.
    The top-right pixel is white because the top layer is 2 (transparent), but the second layer is 1.
    The bottom-left pixel is white because the top two layers are 2, but the third layer is 1.
    The bottom-right pixel is black because the only visible pixel in that position is 0 (from layer 4).
    So, the final image looks like this:
    
    01
    10
    What message is produced after decoding your image?
    
    Your puzzle answer was YLFPJ.

---
+++ 2019/08/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+namespace day08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==== Part 1 ====");
+            var stopwatch = Stopwatch.StartNew();
+            var encodedImage = File
+                .ReadAllText("input.txt");
+
+            var width = 25;
+            var height = 6;
+            var pixels = width * height;
+
+            Console.WriteLine("Total pixels: {0}", encodedImage.Length);
+            Console.WriteLine("Total Layers: {0}", encodedImage.Length / pixels);
+            var layers = DivideIntoLayers(encodedImage, pixels);
+
+            Layer layerWithFewestZeros = layers
+                .OrderBy(l => l.ImageData.Count(d => d == 0))
+                .First();
+
+            int ones = layerWithFewestZeros.ImageData.Count(d => d == 1);
+            int twos = layerWithFewestZeros.ImageData.Count(d => d == 2);
+            
+            stopwatch.Stop();
+            Console.WriteLine("Layer with fewest zeros: {0}", layerWithFewestZeros.LayerNumber);
+            Console.WriteLine("Ones[{0}] x Twos[{1}]: {2}", ones, twos, ones*twos);
+            Console.WriteLine("Calculation took: {0}", stopwatch.Elapsed);
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
+            
+            Console.WriteLine("==== Part 2 ====");
+            stopwatch.Start();
+            var canvas = new int[width, height];
+            layers
+                .OrderByDescending(l => l.LayerNumber)
+                .SelectMany(l => l.ImageData.Select((pixel, index) => new {
+                        X = index % width, 
+                        Y = index / width, 
+                        Pixel = pixel 
+                    }))
+                .Where(p => p.Pixel != 2)
+                .ToList()
+                .ForEach(p => canvas[p.X, p.Y] = p.Pixel);
+
+            canvas.ToConsole(x => x == 0 ? " ": "█");
+
+            stopwatch.Stop();
+            Console.WriteLine("Calculation took: {0}", stopwatch.Elapsed);
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        private static IEnumerable<Layer> DivideIntoLayers(string imgdata, int pixels)
+        {
+            return imgdata
+                .ToList()
+                .Chunk(pixels)
+                .Select((pixelsOfALayer, index) => new Layer(){
+                    LayerNumber = index+1,
+                    ImageData = pixelsOfALayer.Select(c => c.ToInt()).ToArray()
+            });
+        }
+    }
+
+    public class Layer{
+        public int LayerNumber {get; set;}
+        public int[] ImageData {get; set;}
+    }
+}
+public static class Extensions
+{
+    public static void ToConsole<T>(this T[,] canvas, Func<T, string> tranform = null){
+        var innerTransform = tranform ?? new Func<T, string>( (x) => x.ToString());
+        
+        Console.WriteLine($"\r\n[{canvas.GetUpperBound(0)+1},{canvas.GetUpperBound(1)+1}]");
+        for (int y = 0; y <= canvas.GetUpperBound(1); y++)
+        {
+            for (int x = 0; x <= canvas.GetUpperBound(0); x++)
+            {
+                Console.Write(innerTransform(canvas[x,y]));
+            }
+            Console.Write("\r\n");
+        }
+        Console.Write("\r\n");
+    }
+
+    public static IEnumerable<string> Splizz(this string str, params string[] seps){
+        if(seps == null || seps.Length == 0)
+            seps = new[]{";", ","};
+        return str.Split(seps,StringSplitOptions.RemoveEmptyEntries).Select(e => e.Trim());
+    }
+    public static IEnumerable<IEnumerable<string>> Splizz(this IEnumerable<string> enumerable, params string[] seps){
+        foreach (var item in enumerable)
+        {
+            yield return item.Splizz(seps);
+        }
+    }
+
+    public static int ToInt(this char character){
+        return int.Parse(character.ToString());
+    }
+}
+public static class EnumerableExtensions
+{
+
+  public static IEnumerable<IEnumerable<T>> Chunk<T>(this IEnumerable<T> source, int chunkSize)
+  {
+     if (chunkSize == 0)
+        throw new ArgumentNullException();
+
+     var enumer = source.GetEnumerator();
+     while (enumer.MoveNext())
+     {
+        yield return Take(enumer.Current, enumer, chunkSize);
+     }
+  }
+
+  private static IEnumerable<T> Take<T>(T head, IEnumerator<T> tail, int chunkSize)
+  {
+     while (true)
+     {
+        yield return head;
+        if (--chunkSize == 0)
+           break;
+        if (tail.MoveNext())
+           head = tail.Current;
+        else
+           break;
+     }
+  }
+}
+
+
