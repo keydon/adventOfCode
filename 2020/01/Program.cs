d5f7a27f1f67be8ab5dd1351464a7b4aeeebdafc
Lukas Bugaj <lukas.bugaj@cas.de>
2020-12-01 06:34:16 +0100

    --- Day 1: Report Repair --- After saving Christmas five years in a row, you've decided to take a vacation at a nice resort on a tropical island. Surely, Christmas will go on without you.
    The tropical island has its own currency and is entirely cash-only. The gold coins used there have a little picture of a starfish; the locals just call them stars. None of the currency exchanges seem to have heard of them, but somehow, you'll need to find fifty of these coins by the time you arrive so you can pay the deposit on your room.
    
    To save your vacation, you need to get all fifty stars by December 25th.
    
    Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
    
    Before you leave, the Elves in accounting just need you to fix your expense report (your puzzle input); apparently, something isn't quite adding up.
    
    Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.

---
+++ 2020/01/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==== Part 1 ====");
+            var stopwatch = Stopwatch.StartNew();
+            var expenses = LoadExpenseReport("input.txt");
+
+            SolvePartOne(expenses);
+
+            stopwatch.Stop();
+            Console.WriteLine("Calculation took: {0}", stopwatch.Elapsed);
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
+            Console.WriteLine("==== Part 2 ====");
+            stopwatch.Start();
+
+            SolvePartTwo(expenses);
+
+            stopwatch.Stop();
+            Console.WriteLine("Calculation took: {0}", stopwatch.Elapsed);
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        private static void SolvePartOne(List<int> expenses)
+        {
+            foreach (var a in expenses)
+            {
+                foreach (var b in expenses)
+                {
+                    if (a + b == 2020)
+                    {
+                        Console.WriteLine($"Answer is {a * b}");
+                        return;
+                    }
+                }
+            }
+        }
+
+        private static void SolvePartTwo(List<int> expenses)
+        {
+            foreach (var a in expenses)
+            {
+                foreach (var b in expenses)
+                {
+                    foreach (var c in expenses)
+                    {
+                        if (a + b + c == 2020)
+                        {
+                            Console.WriteLine($"Answer is {a * b * c}");
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        public static List<int> LoadExpenseReport(string inputTxt)
+        {
+            return File
+                .ReadAllLines(inputTxt)
+                .Select(s => s.Trim())
+                .Select(int.Parse)
+                .ToList();
+        }
+    }
+}
