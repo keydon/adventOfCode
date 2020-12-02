73096665adf1d5af3685557998514d4397c59281
Lukas Bugaj <lukas.bugaj@cas.de>
2020-12-02 06:46:16 +0100

    --- Day 2: Password Philosophy --- Your flight departs in a few days from the coastal airport; the easiest way down to the coast from here is via toboggan.
    The shopkeeper at the North Pole Toboggan Rental Shop is having a bad day. "Something's wrong with our computers; we can't log in!" You ask if you can take a look.
    
    Their password database seems to be a little corrupted: some of the passwords wouldn't have been allowed by the Official Toboggan Corporate Policy that was in effect when they were chosen.
    
    To try to debug the problem, they have created a list (your puzzle input) of passwords (according to the corrupted database) and the corporate policy when that password was set.
    
    How many passwords are valid according to their policies?
    
    --- Part Two ---
    While it appears you validated the passwords correctly, they don't seem to be what the Official Toboggan Corporate Authentication System is expecting.
    
    The shopkeeper suddenly realizes that he just accidentally explained the password policy rules from his old job at the sled rental place down the street! The Official Toboggan Corporate Policy actually works a little differently.
    
    Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. (Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these positions must contain the given letter. Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
    
    How many passwords are valid according to the new interpretation of the policies?

---
+++ 2020/02/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _02
+{
+    record PasswordPolicy
+    {
+        public int Low { get; set; }
+        public int High { get; set; }
+        public char Letter { get; set; }
+        public string Password { get; set; }
+
+        public bool isValid1(){
+            int occurences = 0;
+            foreach (var l in Password)
+            {
+                if (l == Letter) {
+                    occurences++;
+                }
+            }
+            return Low<= occurences && occurences <= High;
+        }
+        
+        public bool isValid2(){
+            var pos1 = Low - 1;
+            var pos2 = High - 1;
+            bool ok1 = Password[pos1] == Letter;
+            bool ok2 = Password[pos2] == Letter;
+            
+            return ok1 ^ ok2;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foos = LoadFoos("input.txt");
+
+            Console.WriteLine("==== Part 1 ====");
+            var result1 = foos.Count(f => f.isValid1());
+            Console.WriteLine($"Part1-Result: {result1}");
+
+      
+            Console.WriteLine("==== Part 2 =====");
+            var result2 = foos.Count(f => f.isValid2());
+            Console.WriteLine($"Part2-Result: {result2}");
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        public static List<PasswordPolicy> LoadFoos(string inputTxt)
+        {
+            var foos = File
+                .ReadAllLines(inputTxt)
+                .Select(r => r.Splizz(",", ";", "-", ":", " ").ToArray())
+                .Select(s => new PasswordPolicy() { 
+                    Low = int.Parse(s[0]), 
+                    High = int.Parse(s[1]),
+                    Letter= s[2][0],
+                    Password=s[3]
+                })
+                .ToList();
+            return foos;
+        }
+    }
+    public static class Extensions
+    {
+        public static IEnumerable<string> Splizz(this string str, params string[] seps)
+        {
+            if (seps == null || seps.Length == 0)
+                seps = new[] { ";", "," };
+            return str.Split(seps, StringSplitOptions.RemoveEmptyEntries).Select(e => e.Trim());
+        }
+    }
+}
