a0c027e71f1254347bd7115346b1958ed6b46fdf
Lukas Bugaj <lukas.bugaj@cas.de>
2020-12-03 07:17:10 +0100

    --- Day 3: Toboggan Trajectory --- With the toboggan login problems resolved, you set off toward the airport. While travel by toboggan might be easy, it's certainly not safe: there's very minimal steering and the area is covered in trees. You'll need to see which angles will take you near the fewest trees.
    Due to the local geology, trees in this area only grow on exact integer coordinates in a grid. You make a map (your puzzle input) of the open squares (.) and trees (#) you can see.
    
    These aren't the only trees, though; due to something you read about once involving arboreal genetics and biome stability, the same pattern repeats to the right many times
    
    You start on the open square (.) in the top-left corner and need to reach the bottom (below the bottom-most row on your map).
    
    The toboggan can only follow a few specific slopes (you opted for a cheaper model that prefers rational numbers); start by counting all the trees you would encounter for the slope right 3, down 1.
    
    --- Part Two ---
    Time to check the rest of the slopes - you need to minimize the probability of a sudden arboreal stop, after all.
    
    Determine the number of trees you would encounter if, for each of the following slopes, you start at the top-left corner and traverse the map all the way to the bottom:
    
    Right 1, down 1.
    Right 3, down 1. (This is the slope you already checked.)
    Right 5, down 1.
    Right 7, down 1.
    Right 1, down 2.
    In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s) respectively; multiplied together, these produce the answer 336.
    
    What do you get if you multiply together the number of trees encountered on each of the listed slopes?

---
+++ 2020/03/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+
+namespace _03
+{
+    record Field
+    {
+        public string Type { get; set; }
+        public Point Position {get; set; }
+
+        public bool IsTree(){
+            return Type == "#";
+        }
+    }
+    class Program
+    {
+        private static Dictionary<Point, Field> fieldLookup;
+        private static int maxX;
+        private static int maxY;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==== Part 1 & 2 ====");
+            var foos = LoadMap("input.txt");
+            maxX = foos.Max(f => f.Position.X);
+            maxY = foos.Max(f => f.Position.Y);
+            fieldLookup = foos.ToDictionary(f => f.Position);
+            Console.WriteLine($"maxX: {maxX}, maxY: {maxY}");      
+
+            var slopes = new Point[] {new Point(1,1), new Point(3, 1), new Point(5, 1), new Point(7, 1), new Point(1,2)}; 
+
+            var result1 = slopes
+                .Select(s => CountTrees(s))
+                .Peak()
+                .Aggregate(1, (a,b) => a * b);
+           
+            Console.WriteLine($"Part2-Result: {result1}");
+        }
+
+        static int CountTrees(Point slope){
+            return Traverse(slope).Count(f => f.IsTree());
+        }
+
+        static IEnumerable<Field> Traverse(Point slope){
+            var field = fieldLookup[new Point(0,0)];
+            
+            while (true) {
+                yield return field;
+                field = MoveBySlope(field.Position, slope);
+                if (field == null) {
+                    break; // reached bottom
+                }
+            }
+        }
+
+        static Field MoveBySlope(Point pos, Point slope) {
+            var newPos = new Point(pos.X + slope.X, pos.Y + slope.Y);
+            if(newPos.Y > maxY) {
+                return null;
+            }
+            if(newPos.X > maxX) {
+                // hups, out bounds. just start from the left again
+                newPos = new Point(newPos.X - maxX - 1, newPos.Y);
+            }
+            return fieldLookup[newPos];
+        }
+
+        public static List<Field> LoadMap(string inputTxt, int top = 0)
+        {
+            var fields = File
+                .ReadAllLines(inputTxt)
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .Select(s => s.Trim())
+                .SelectMany((row, y) => row.Select((fieldType, x) => new Field { Position = new Point(x, y), Type = fieldType.ToString() }));
+
+            var fieldsList = fields.ToList();
+            Console.WriteLine($"Loaded {fields.Count()} entries ({inputTxt})");
+            return fieldsList;
+        }
+    }
+    public static class Extensions
+    {
+        public static IEnumerable<T> Peak<T>(this IEnumerable<T> enumerable, string prefix = ""){
+            return enumerable.Select(x => {
+                Console.WriteLine($"Peak: {prefix} {x}");
+                return x;
+            });
+        }
+    }
+}
