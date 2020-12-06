1a310d308f4d17b5fb31b6247502104cd18898cb
Lukas Bugaj <lukas.bugaj@cas.de>
2020-12-06 07:07:51 +0100

    --- Day 6: Custom Customs --- As your flight approaches the regional airport where you'll switch to a much larger plane, customs declaration forms are distributed to the passengers.
    The form asks a series of 26 yes-or-no questions marked a through z. All you need to do is identify the questions for which anyone in your group answers "yes". Since your group is just you, this doesn't take very long.
    
    However, the person sitting next to you seems to be experiencing a language barrier and asks if you can help. For each of the people in their group, you write down the questions for which they answer "yes", one per line. For example:
    
    abcx
    abcy
    abcz
    In this group, there are 6 questions to which anyone answered "yes": a, b, c, x, y, and z. (Duplicate answers to the same question don't count extra; each question counts at most once.)
    
    Another group asks for your help, then another, and eventually you've collected answers from every group on the plane (your puzzle input). Each group's answers are separated by a blank line, and within each group, each person's answers are on a single line.
    
    --- Part Two ---
    As you finish the last group's customs declaration, you notice that you misread one word in the instructions:
    
    You don't need to identify the questions to which anyone answered "yes"; you need to identify the questions to which everyone answered "yes"!

---
+++ 2020/06/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Reflection;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using TextCopy;
+
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==== Part 1 ====");
+            var groups = LoadGroupAnswers("input.txt");
+
+            var result1 = groups
+                .Select(g => g.SelectMany(p => p).Distinct())
+                .Sum(g => g.Count());
+
+            Console.WriteLine($"Part1-Result: {result1}");
+
+
+            // First working solution of part II
+            /* int sum = 0;
+             foreach (var foo in groups)
+             {
+                 var res = foo.SelectMany(g => g).Distinct().ToList();
+
+                 foreach (var g in foo)
+                 {
+                     res = res.Intersect(g).ToList();
+                 }
+                 sum += res.Count();
+             }*/
+
+            var result2 = groups
+                .Select(g => new
+                {
+                    allAnswers = g.SelectMany(a => a),
+                    Group = g
+                })
+                .Select(g => g.Group.Aggregate(g.allAnswers, (unanimous, answers) => unanimous.Intersect(answers)))
+                .Sum(g => g.Count());
+
+            Console.WriteLine($"Part2-Result: {result2}");
+        }
+
+        public static List<List<List<char>>> LoadGroupAnswers(string inputTxt, int top = 0)
+        {
+            const string GROUP_SEPERATOR = "";
+            var groups = File
+                .ReadAllLines(inputTxt)
+                .Select(s => s.Trim())
+                .Aggregate(new LinkedList<List<List<char>>>(), (groups, personsQuestions) =>
+                {
+                    if (personsQuestions == GROUP_SEPERATOR || groups.Count == 0)
+                    {
+                        groups.AddLast(new List<List<char>>());
+
+                    }
+
+                    var group = groups.Last.Value;
+
+                    if (personsQuestions != GROUP_SEPERATOR)
+                    {
+                        var questions = personsQuestions.Select(c => c).ToList();
+                        group.Add(questions);
+                    }
+
+                    return groups;
+                });
+            ;
+
+            var groupsList = groups.ToList();
+            Console.WriteLine($"Loaded {groups.Count()} entries ({inputTxt})");
+            return groupsList;
+        }
+    }
+}
