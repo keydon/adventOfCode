5ed029ab1014ed570e708215e63bd10541d446cf
Lukas Bugaj <lukas.bugaj@cas.de>
2020-12-09 07:15:00 +0100

    --- Day 9: Encoding Error --- With your neighbor happily enjoying their video game, you turn your attention to an open data port on the little screen in the seat in front of you.
    Though the port is non-standard, you manage to connect it to your computer through the clever use of several paperclips. Upon connection, the port outputs a series of numbers (your puzzle input).
    
    The data appears to be encrypted with the eXchange-Masking Addition System (XMAS) which, conveniently for you, is an old cypher with an important weakness.
    
    XMAS starts by transmitting a preamble of 25 numbers. After that, each number you receive should be the sum of any two of the 25 immediately previous numbers. The two numbers will have different values, and there might be more than one such pair.
    
    The first step of attacking the weakness in the XMAS data is to find the first number in the list (after the preamble) which is not the sum of two of the 25 numbers before it. What is the first number that does not have this property?
    
    --- Part Two ---
    The final step in breaking the XMAS encryption relies on the invalid number you just found: you must find a contiguous set of at least two numbers in your list which sum to the invalid number from step 1.
    
    To find the encryption weakness, add together the smallest and largest number in this contiguous range.
    
    What is the encryption weakness in your XMAS-encrypted list of numbers?

---
+++ 2020/09/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace _09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foos = LoadFoos("input.txt");
+
+            var result1 = CalculatePart1(foos);
+            Console.WriteLine($"Part1-Result: {result1}");
+
+            var result2 = CalculatePart2(foos, result1);
+            Console.WriteLine($"Part2-Result: {result2}");
+        }
+
+        private static long CalculatePart2(List<long> foos, long result1)
+        {
+            for (int i = 0; i < foos.Count; i++)
+            {
+                var take = 2;
+                long sum = 0;
+                while (sum < result1)
+                {
+                    var candidates = foos.Skip(i).Take(take);
+                    sum = candidates.Sum();
+                    if (sum == result1)
+                    {
+                        return candidates.Min() + candidates.Max();
+                    }
+                    take++;
+                }
+            }
+
+            throw new Exception("Cannot find the solution for part II :/");
+        }
+
+        private static long CalculatePart1(List<long> foos)
+        {
+            var preamble = 25;
+            for (int i = preamble; i < foos.Count; i++)
+            {
+                var candidate = foos[i];
+                var possible = foos.Skip(i - preamble).Take(preamble).ToList();
+                if (possible.Any(x => possible.Contains(candidate - x)))
+                {
+                    continue;
+                }
+                return candidate;
+            }
+            throw new Exception("Cannot find the solution for part I");
+        }
+
+        public static List<long> LoadFoos(string inputTxt, int top = 0)
+        {
+            var foos = File
+                .ReadAllLines(inputTxt)
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .Select(s => s.Trim())
+                .Select(long.Parse);
+
+            var foosList = foos.ToList();
+            Console.WriteLine($"Loaded {foos.Count()} entries ({inputTxt})");
+            return foosList;
+        }
+    }
+}
