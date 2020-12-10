c5110cda22c2f7d0cdff62109e058d2911f6dde6
Lukas Bugaj <lukas.bugaj@cas.de>
2020-12-10 07:58:00 +0100

    --- Day 10: Adapter Array --- Patched into the aircraft's data port, you discover weather forecasts of a massive tropical storm. Before you can figure out whether it will impact your vacation plans, however, your device suddenly turns off!
    Its battery is dead.
    
    You'll need to plug it in. There's only one problem: the charging outlet near your seat produces the wrong number of jolts. Always prepared, you make a list of all of the joltage adapters in your bag.
    
    Each of your joltage adapters is rated for a specific output joltage (your puzzle input). Any given adapter can take an input 1, 2, or 3 jolts lower than its rating and still produce its rated output joltage.
    
    In addition, your device has a built-in joltage adapter rated for 3 jolts higher than the highest-rated adapter in your bag. (If your adapter list were 3, 9, and 6, your device's built-in adapter would be rated for 12 jolts.)
    
    Treat the charging outlet near your seat as having an effective joltage rating of 0.
    
    Since you have some time to kill, you might as well test all of your adapters. Wouldn't want to get to your resort and realize you can't even charge your device!
    
    If you use every adapter in your bag at once, what is the distribution of joltage differences between the charging outlet, the adapters, and your device?
    
    Find a chain that uses all of your adapters to connect the charging outlet to your device's built-in adapter and count the joltage differences between the charging outlet, the adapters, and your device. What is the number of 1-jolt differences multiplied by the number of 3-jolt differences?
    
    --- Part Two ---
    To completely determine whether you have enough adapters, you'll need to figure out how many different ways they can be arranged. Every arrangement needs to connect the charging outlet to your device. The previous rules about when adapters can successfully connect still apply.
    
    You glance back down at your bag and try to remember why you brought so many adapters; there must be more than a trillion valid ways to arrange them! Surely, there must be an efficient way to count the arrangements.
    
    What is the total number of distinct ways you can arrange the adapters to connect the charging outlet to your device?

---
+++ 2020/10/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+
+namespace _01
+{
+    record IndexedJoltage
+    {
+        public int Jolts { get; set; }
+        public int Index { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==== Part 1 ====");
+            var stopwatch = Stopwatch.StartNew();
+            var foos = LoadJoltageRatings("input.txt");
+
+            foos.Add(0); // charging outlet
+            foos.Add(foos.Max() + 3); // built-in device
+            foos.Sort();
+
+            var stepCount = new Dictionary<int, int>();
+            var sorted = foos.Aggregate(0, (a, b) =>
+            {
+                var step = b - a;
+                stepCount[step] = stepCount.GetValueOrDefault(step, 0) + 1;
+                return b;
+            });
+
+            var result1 = stepCount[1] * stepCount[3];
+            Console.WriteLine($"Part1-Result: {result1}");
+
+            var memo = new Dictionary<IndexedJoltage, long>();
+            var indexedJoltages = foos.Select((j, i) => new IndexedJoltage() { Jolts = j, Index = i }).ToList();
+
+            memo[indexedJoltages.Last()] = 1;
+            long possis = CalcPossibilities(indexedJoltages, indexedJoltages.First(), memo);
+            Console.WriteLine($"Part2-Result: {possis}");
+        }
+
+        public static long CalcPossibilities(List<IndexedJoltage> ratings, IndexedJoltage current, Dictionary<IndexedJoltage, long> memo)
+        {
+            if (memo.ContainsKey(current))
+            {
+                return memo[current];
+            }
+            var res = ratings.Skip(current.Index + 1)
+                .Take(3)
+                .Where((next) => (next.Jolts - current.Jolts) <= 3)
+                .Select(x => CalcPossibilities(ratings, x, memo))
+                .Sum();
+            memo[current] = res;
+            return res;
+        }
+
+        public static List<int> LoadJoltageRatings(string inputTxt)
+        {
+            var foos = File
+                .ReadAllLines(inputTxt)
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .Select(s => s.Trim())
+                .Select(int.Parse);
+
+            var foosList = foos.ToList();
+            Console.WriteLine($"Loaded {foos.Count()} entries ({inputTxt})");
+            return foosList;
+        }
+    }
+}
