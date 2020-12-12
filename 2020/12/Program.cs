8f3dd910d1d1b35d009bf27b1114a634b38ada84
Lukas Bugaj <lukas.bugaj@cas.de>
2020-12-12 10:10:56 +0100

    --- Day 12: Rain Risk --- Your ferry made decent progress toward the island, but the storm came in faster than anyone expected. The ferry needs to take evasive actions!
    Unfortunately, the ship's navigation computer seems to be malfunctioning; rather than giving a route directly to safety, it produced extremely circuitous instructions. When the captain uses the PA system to ask if anyone can help, you quickly volunteer.
    
    The navigation instructions (your puzzle input) consists of a sequence of single-character actions paired with integer input values. After staring at them for a few minutes, you work out what they probably mean:
    
    Action N means to move north by the given value.
    Action S means to move south by the given value.
    Action E means to move east by the given value.
    Action W means to move west by the given value.
    Action L means to turn left the given number of degrees.
    Action R means to turn right the given number of degrees.
    Action F means to move forward by the given value in the direction the ship is currently facing.
    The ship starts by facing east. Only the L and R actions change the direction the ship is facing. (That is, if the ship is facing east and the next instruction is N10, the ship would move north 10 units, but would still move east if the following action were F.)
    
    Figure out where the navigation instructions lead. What is the Manhattan distance between that location and the ship's starting position?
    
    --- Part Two ---
    Before you can give the destination to the captain, you realize that the actual action meanings were printed on the back of the instructions the whole time.
    
    Almost all of the actions indicate how to move a waypoint which is relative to the ship's position:
    
    Action N means to move the waypoint north by the given value.
    Action S means to move the waypoint south by the given value.
    Action E means to move the waypoint east by the given value.
    Action W means to move the waypoint west by the given value.
    Action L means to rotate the waypoint around the ship left (counter-clockwise) the given number of degrees.
    Action R means to rotate the waypoint around the ship right (clockwise) the given number of degrees.
    Action F means to move forward to the waypoint a number of times equal to the given value.
    The waypoint starts 10 units east and 1 unit north relative to the ship. The waypoint is relative to the ship; that is, if the ship moves, the waypoint moves with it.
    
    Figure out where the navigation instructions actually lead. What is the Manhattan distance between that location and the ship's starting position?

---
+++ 2020/12/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Reflection;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using TextCopy;
+
+namespace aoc
+{
+    record Pointish
+    {
+        public Direction Facing { get; set; }
+        public Point Pos { get; set; }
+
+        public void Move(Direction direction, int steps)
+        {
+            Pos = Pos.Move(direction, steps);
+        }
+        public void Move(Point waypoint, int steps)
+        {
+            Pos = Pos.Move(waypoint, steps);
+        }
+        public void RotateFacing(Rotation rotation)
+        {
+            Facing = Facing.Rotate(rotation);
+        }
+        public void RotateOrientation(Rotation rotation)
+        {
+            Pos = Pos.Rotate(rotation);
+        }
+    }
+    record Movement
+    {
+        public int Amount { get; set; }
+        public string Act { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var moves = LoadMovements("input.txt");
+            var ship1 = new Pointish() { Facing = Direction.RIGHT };
+            foreach (var move in moves)
+            {
+                MovePart1Style(ship1, move);
+
+            }
+            ship1.Pos.ManhattenDistance().AsResult1();
+
+            var ship2 = new Pointish();
+            var waypoint = new Pointish() { Pos = new Point(0, 0).Right(10).Up(1) };
+            foreach (var move in moves)
+            {
+                MovePart2Style(ship2, move, waypoint);
+
+            }
+            ship2.Pos.ManhattenDistance().AsResult2();
+            Report.End();
+        }
+
+        private static void MovePart2Style(Pointish ship, Movement move, Pointish waypoint)
+        {
+            switch (move.Act)
+            {
+                case "F":
+                    ship.Move(waypoint.Pos, move.Amount);
+                    break;
+                case "E":
+                case "N":
+                case "W":
+                case "S":
+                    var direction = PointParseUtils.ParseDirection(move.Act);
+                    waypoint.Move(direction, move.Amount);
+                    break;
+                case "L":
+                case "R":
+                    var rotation = PointParseUtils.ParseRotation(move.Act, move.Amount);
+                    waypoint.RotateOrientation(rotation);
+                    break;
+                default:
+                    throw new Exception("Unknown act: " + move.Act);
+            }
+        }
+
+        private static void MovePart1Style(Pointish ship, Movement move)
+        {
+            switch (move.Act)
+            {
+                case "F":
+                    ship.Move(ship.Facing, move.Amount);
+                    break;
+                case "E":
+                case "N":
+                case "W":
+                case "S":
+                    var direction = PointParseUtils.ParseDirection(move.Act);
+                    ship.Move(direction, move.Amount);
+                    break;
+                case "L":
+                case "R":
+                    var rotation = PointParseUtils.ParseRotation(move.Act, move.Amount);
+                    ship.RotateFacing(rotation);
+                    break;
+                default:
+                    throw new Exception("Act not impl: " + move.Act);
+            }
+        }
+
+        public static List<Movement> LoadMovements(string inputTxt)
+        {
+            var foos = File
+                .ReadAllLines(inputTxt)
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .Select(s => s.Trim())
+                .Select(s => ParseRegex(s));
+
+            var foosList = foos.ToList();
+            Console.WriteLine($"Loaded {foos.Count()} entries ({inputTxt})");
+            return foosList;
+        }
+
+        private static Movement ParseRegex(string line)
+        {
+            Regex operationRegEx = new Regex(@"^([A-Z])(\d+)$");
+            var match = operationRegEx.Match(line);
+            if (!match.Success)
+                throw new Exception("No RegEx-Match for: " + line);
+
+            return new Movement()
+            {
+                Amount = int.Parse(match.Groups[2].Value),
+                Act = match.Groups[1].Value,
+            };
+        }
+    }
+}
