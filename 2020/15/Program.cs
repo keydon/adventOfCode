9f0ccb499e9736855e189e56fb391b54a34160bb
Lukas Bugaj <lukas.bugaj@cas.de>
2020-12-15 07:17:34 +0100

    --- Day 15: Rambunctious Recitation --- You catch the airport shuttle and try to book a new flight to your vacation island. Due to the storm, all direct flights have been cancelled, but a route is available to get around the storm. You take it.
    While you wait for your flight, you decide to check in with the Elves back at the North Pole. They're playing a memory game and are ever so excited to explain the rules!
    
    In this game, the players take turns saying numbers. They begin by taking turns reading from a list of starting numbers (your puzzle input). Then, each turn consists of considering the most recently spoken number:
    
    If that was the first time the number has been spoken, the current player says 0.
    Otherwise, the number had been spoken before; the current player announces how many turns apart the number is from when it was previously spoken.
    So, after the starting numbers, each turn results in that player speaking aloud either 0 (if the last number is new) or an age (if the last number is a repeat).
    
    The game ends when the Elves get sick of playing or dinner is ready, whichever comes first.)
    
    Their question for you is: what will be the 2020th number spoken?
    
    --- Part Two ---
    
    Impressed, the Elves issue you a challenge: determine the 30000000th number spoken

---
+++ 2020/15/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace aoc
+{
+    record Spoken
+    {
+        public int Number { get; set; }
+        public int Position { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = LoadStartingNumbers("input.txt");
+            GetNthSpokenNumber(numbers, 2020).AsResult1();
+            GetNthSpokenNumber(numbers, 30000000).AsResult2();
+            Report.End();
+        }
+
+        private static int GetNthSpokenNumber(IEnumerable<Spoken> foos, int theNthNumber)
+        {
+            var spoken = foos.SkipLast(1).ToDictionary(n => n.Number, n => n.Position);
+
+            var lastSpoken = foos.Last().Number;
+            var count = foos.Count();
+            while (true)
+            {
+                var lastPosition = count;
+                count++;
+
+                if (spoken.TryGetValue(lastSpoken, out var lastBeforePosition))
+                {
+                    spoken[lastSpoken] = lastPosition;
+                    var age = lastPosition - lastBeforePosition;
+                    lastSpoken = age;
+                }
+                else
+                {
+                    spoken[lastSpoken] = lastPosition;
+                    lastSpoken = 0;
+                }
+                if (count == theNthNumber)
+                {
+                    return lastSpoken;
+                }
+            }
+        }
+
+        public static IEnumerable<Spoken> LoadStartingNumbers(string inputTxt, int top = 0)
+        {
+            var foos = File
+                .ReadAllLines(inputTxt)
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .Select(s => s.Trim())
+                .SelectMany(r => r.Splizz(",", ";")
+                    .Select((x, i) => new Spoken() { Number = int.Parse(x), Position = i + 1 }));
+
+            return foos;
+        }
+    }
+
+    public static class StringExtensions
+    {
+        public static IEnumerable<string> Splizz(this string str, params string[] seps)
+        {
+            if (seps == null || seps.Length == 0)
+                seps = new[] { ";", "," };
+            return str.Split(seps, StringSplitOptions.RemoveEmptyEntries).Select(e => e.Trim());
+        }
+    }
+}
