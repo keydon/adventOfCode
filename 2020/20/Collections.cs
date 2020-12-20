a86fd799197f1370c86acd15c59dbdc5fcfe9d4a
Lukas Bugaj <lukas.bugaj@cas.de>
2020-12-20 07:31:18 +0100

    --- Day 20: Jurassic Jigsaw --- The high-speed train leaves the forest and quickly carries you south. You can even see a desert in the distance! Since you have some spare time, you might as well see if there was anything interesting in the image the Mythical Information Bureau satellite captured.
    After decoding the satellite messages, you discover that the data actually contains many small images created by the satellite's camera array. The camera array consists of many cameras; rather than produce a single square image, they produce many smaller square image tiles that need to be reassembled back into a single image.
    
    Each camera in the camera array returns a single monochrome image tile with a random unique ID number. The tiles (your puzzle input) arrived in a random order.
    
    Worse yet, the camera array appears to be malfunctioning: each image tile has been rotated and flipped to a random orientation. Your first task is to reassemble the original image by orienting the tiles so they fit together.
    
    To show how the tiles should be reassembled, each tile's image data includes a border that should line up exactly with its adjacent tiles. All tiles have this border, and the border lines up exactly when the tiles are both oriented correctly. Tiles at the edge of the image also have this border, but the outermost edges won't line up with any other tiles.
    
    o check that you've assembled the image correctly, multiply the IDs of the four corner tiles together.

---
+++ 2020/20/Collections.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace aoc
+{
+    public static class EnumberableCollectionsExtensions
+    {
+        public static long MultiplyAll(this IEnumerable<int> numbers)
+        {
+            return numbers.Select(n => (long)n).Aggregate((a, b) => a * b);
+        }
+        public static long MultiplyAll(this IEnumerable<long> numbers)
+        {
+            return numbers.Aggregate((a, b) => a * b);
+        }
+        public static long MultiplyMany<T>(this IEnumerable<T> items, Func<T, long> longSelector)
+        {
+            return items.Select(longSelector).Aggregate((a, b) => a * b);
+        }
+
+        public static IEnumerable<T> IntersectMany<T>(this IEnumerable<IEnumerable<T>> enumberable)
+        {
+            var items = enumberable.ToList();
+            var allItems = enumberable.SelectMany(i => i).ToList();
+
+            return items.Aggregate(allItems, (intersect, next) => intersect.Intersect(next).ToList());
+        }
+        public static IEnumerable<T> UnionMany<T>(this IEnumerable<IEnumerable<T>> enumberable)
+        {
+            return enumberable.Aggregate(new List<T>(), (union, next) => union.Union(next).ToList());
+        }
+
+        public static IEnumerable<IEnumerable<T>> Chunk<T>(this IEnumerable<T> source, int chunkSize)
+        {
+            if (chunkSize == 0)
+                throw new ArgumentNullException();
+
+            var enumer = source.GetEnumerator();
+            while (enumer.MoveNext())
+            {
+                yield return Take(enumer.Current, enumer, chunkSize);
+            }
+        }
+
+        private static IEnumerable<T> Take<T>(T head, IEnumerator<T> tail, int chunkSize)
+        {
+            while (true)
+            {
+                yield return head;
+                if (--chunkSize == 0)
+                    break;
+                if (tail.MoveNext())
+                    head = tail.Current;
+                else
+                    break;
+            }
+        }
+    }
+}
---
+++ 2020/20/Program.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace aoc
+{
+    enum Ori
+    {
+        x0, x0r, y0, y0r, xM, xMR, yM, yMR
+    }
+    record Foo<TPoint> : IHasPosition<TPoint>
+        where TPoint : IPointish
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public string A { get; set; }
+        public string B { get; set; }
+
+        public TPoint Pos { get; set; }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Report.Start();
+            var foos = LoadFoos("input.txt");
+            //foos = LoadFoos("sample.txt");
+
+            foreach (var f in foos)
+            {
+                f.Borders = CalcBorders(f);
+            }
+            var fieldDic = foos.ToDictionary(f => f.Id);
+
+            var borderDic = new Dictionary<string, List<Field<Point2, Foo<Point2>>>>();
+            foreach (var f in foos)
+            {
+                MatchBorders(borderDic, f, foos);
+            }
+            var bc = foos.Select(f =>
+                  (f.Borders
+                      .Select(b => b.border)
+                      .Select(b => borderDic[b].Count)
+                      .Sum(), f.Id)
+                );
+            var min = bc.Min(p => p.Item1);
+            var max = bc.Max(p => p.Item1);
+            var mins = bc.Where(b => b.Item1 == min).ToList();
+            if (mins.Count == 4)
+            {
+                mins.Select(p => p.Id).MultiplyAll().AsResult1();
+            }
+
+            var bigTile = BuildBigTile(borderDic, fieldDic, foos, fieldDic[mins.First().Id]);
+
+
+
+
+            Report.End();
+        }
+
+        private static object BuildBigTile(Dictionary<string, List<Field<Point2, Foo<Point2>>>> borderDic, Dictionary<int, Field<Point2, Foo<Point2>>> fieldDic, List<Field<Point2, Foo<Point2>>> foos, Field<Point2, Foo<Point2>> field)
+        {
+            var bigTile = new Field<Point2, Foo<Point2>>(OutOfBoundsStrategy.RETURN_NULL);
+            bigTile.Add(StripBorder(field));
+        }
+
+        private static Field<Point2, Foo<Point2>> StripBorder(Field<Point2, Foo<Point2>> f)
+        {
+
+            var minx
+
+            var field = new Field<Point2, Foo<Point2>>(f.OutOfBoundsStrategy);
+            for (int x = f.MinX; x <= f.MaxX; x++)
+            {
+                for (int y = f.MinY; y <= f.MaxY; y++)
+                {
+                    if (x == 0 || y == 0 || x == f.MaxX || y == f.MaxY)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        var foo = f.Dic[new Point2(x, y)];
+                        yield return foo;
+                    }
+                }
+            }
+        }
+
+        private static object MatchBorders(Dictionary<string, List<Field<Point2, Foo<Point2>>>> dic, Field<Point2, Foo<Point2>> f, List<Field<Point2, Foo<Point2>>> foos)
+        {
+            foreach (var b in f.Borders)
+            {
+                var bs = b.border;
+                if (dic.TryGetValue(bs, out var bl))
+                {
+                    bl.Add(f);
+                }
+                else
+                {
+                    dic.Add(bs, new List<Field<Point2, Foo<Point2>>>() { f });
+                }
+            }
+            return dic;
+        }
+
+        private static List<(string border, Ori orientation)> CalcBorders(Field<Point2, Foo<Point2>> f)
+        {
+            var x0 = new List<string>();
+            var y0 = new List<string>();
+            var xM = new List<string>();
+            var yM = new List<string>();
+
+            for (int x = f.MinX; x <= f.MaxX; x++)
+            {
+                for (int y = f.MinY; y <= f.MaxY; y++)
+                {
+                    var foo = f.Dic[new Point2(x, y)];
+                    if (x == 0)
+                    {
+                        x0.Add(foo.A);
+                    }
+                    if (y == 0)
+                    {
+                        y0.Add(foo.A);
+                    }
+                    if (x == f.MaxX)
+                    {
+                        xM.Add(foo.A);
+                    }
+                    if (y == f.MaxY)
+                    {
+                        yM.Add(foo.A);
+                    }
+                }
+            }
+            var res = new List<(string border, Ori orientation)>
+            {
+                (x0.ToCommaString(""), Ori.x0),
+                (x0.Select(s => s).Reverse().ToCommaString(""), Ori.x0r),
+                (y0.ToCommaString(""), Ori.y0),
+               (y0.Select(s => s).Reverse().ToCommaString(""), Ori.y0r),
+
+                (xM.ToCommaString(""), Ori.xM),
+               (xM.Select(s => s).Reverse().ToCommaString(""), Ori.xMR),
+
+                (yM.ToCommaString(""), Ori.yM),
+               (yM.Select(s => s).Reverse().ToCommaString(""), Ori.yMR),
+            };
+            return res.Debug(f.Id);
+        }
+
+        public static List<Field<Point2, Foo<Point2>>> LoadFoos(string inputTxt)
+        {
+            var foos = File
+                .ReadAllLines(inputTxt)
+                .Select(s => s.Trim())
+                .GroupByLineSeperator()
+                .Select(g =>
+                {
+                    var id = int.Parse(g.First().Splizz(" ").Select(s => s.Trim(':')).Last());
+                    var field = new Field<Point2, Foo<Point2>>(OutOfBoundsStrategy.RETURN_NULL);
+                    field.Add(g.Skip(1).Parse2DMap((p, t) => new Foo<Point2>() { A = t, Pos = p }));
+                    field.Id = id;
+                    return field;
+                })
+
+             //.GroupByLineSeperator()
+             //.Parse2DMap((p, t) => new Foo<Point2> { Pos = p, A = t })
+             //.SelectMany(r => r.Splizz(",", ";"))
+             //.Where(a => a.foo == '#')
+             //.Select(int.Parse)
+             //.Select(long.Parse)  
+             //  .Select(s => s.ParseRegex(@"^mem\[(\d+)\] = (\d+)$", m => new Foo<Point2>()
+             //  {
+             //      X = int.Parse(m.Groups[1].Value),
+             //      Y = int.Parse(m.Groups[2].Value),
+             //  }))
+             //.Where(f = f)
+             //.ToDictionary(
+             //    (a) => new Vector3(a.x, a.y),
+             //    (a) => new Foo(new Vector3(a.x, a.y))
+             //);
+             //.ToArray()
+             .ToList()
+            ;
+
+            return foos;
+        }
+    }
+}
