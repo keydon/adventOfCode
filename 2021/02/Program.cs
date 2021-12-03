e707ae50413fda935ea04d2e52b758134d1d0281
Lukas Bugaj <lukas.bugaj@cas.de>
2021-12-03 07:06:09 +0100

    --- Day 2: Dive! --- (Part I) Now, you need to figure out how to pilot this thing.
    It seems like the submarine can take a series of commands like forward 1, down 2, or up 3:
    
    forward X increases the horizontal position by X units.
    down X increases the depth by X units.
    up X decreases the depth by X units.
    Note that since you're on a submarine, down and up affect your depth, and so they have the opposite result of what you might expect.
    
    The submarine seems to already have a planned course (your puzzle input). You should probably figure out where it's going. For example:
    
    forward 5
    down 5
    forward 8
    up 3
    down 8
    forward 2
    Your horizontal position and depth both start at 0. The steps above would then modify them as follows:
    
    forward 5 adds 5 to your horizontal position, a total of 5.
    down 5 adds 5 to your depth, resulting in a value of 5.
    forward 8 adds 8 to your horizontal position, a total of 13.
    up 3 decreases your depth by 3, resulting in a value of 2.
    down 8 adds 8 to your depth, resulting in a value of 10.
    forward 2 adds 2 to your horizontal position, a total of 15.
    After following these instructions, you would have a horizontal position of 15 and a depth of 10. (Multiplying these together produces 150.)
    
    Calculate the horizontal position and depth you would have after following the planned course. What do you get if you multiply your final horizontal position by your final depth?

---
+++ 2021/02/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace aoc
+{
+    record Command
+    {
+        public int NumericValue { get; set; }
+
+        public string CommandId { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Report.Start();
+            var commands = LoadCommands("input.txt");
+            var dist = 0;
+            var depth = 0;
+            foreach (var instr in commands)
+            {
+                switch(instr.CommandId){
+                    case "up": depth -= instr.NumericValue;
+                        break;
+                    case "down": depth += instr.NumericValue;
+                        break;
+                    case "forward": dist += instr.NumericValue;
+                        break;
+                }
+            }
+
+
+            (dist * depth).AsResult1();
+            Report.End();
+        }
+
+        public static List<Command> LoadCommands(string inputTxt)
+        {
+            var foos = File
+                .ReadAllLines(inputTxt)
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .Select(s => s.Trim())
+                .Select(s => s.ParseRegex(@"^(.+) (\d+)$", m => new Command(){
+                   CommandId = m.Groups[1].Value,
+                   NumericValue = int.Parse(m.Groups[2].Value),
+                }))
+                .ToList();
+
+            return foos;
+        }
+    }
+}
