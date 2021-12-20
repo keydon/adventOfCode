9212786bafb4d8138afd993eae8ac37af378122b
Lukas Bugaj <lukas.bugaj@cas.de>
2021-12-20 09:09:52 +0100

    --- Day 14: Extended Polymerization --- The incredible pressures at this depth are starting to put a strain on your submarine. The submarine has polymerization equipment that would produce suitable materials to reinforce the submarine, and the nearby volcanically-active caves should even have the necessary input elements in sufficient quantities.
    The submarine manual contains instructions for finding the optimal polymer formula; specifically, it offers a polymer template and a list of pair insertion rules (your puzzle input). You just need to work out what polymer would result after repeating the pair insertion process a few times.
    
    For example:
    
    NNCB
    
    CH -> B
    HH -> N
    CB -> H
    NH -> C
    HB -> C
    HC -> B
    HN -> C
    NN -> C
    BH -> H
    NC -> B
    NB -> B
    BN -> B
    BB -> N
    BC -> B
    CC -> N
    CN -> C
    The first line is the polymer template - this is the starting point of the process.
    
    The following section defines the pair insertion rules. A rule like AB -> C means that when elements A and B are immediately adjacent, element C should be inserted between them. These insertions all happen simultaneously.
    
    So, starting with the polymer template NNCB, the first step simultaneously considers all three pairs:
    
    The first pair (NN) matches the rule NN -> C, so element C is inserted between the first N and the second N.
    The second pair (NC) matches the rule NC -> B, so element B is inserted between the N and the C.
    The third pair (CB) matches the rule CB -> H, so element H is inserted between the C and the B.
    Note that these pairs overlap: the second element of one pair is the first element of the next pair. Also, because all pairs are considered simultaneously, inserted elements are not considered to be part of a pair until the next step.
    
    After the first step of this process, the polymer becomes NCNBCHB.
    
    Here are the results of a few steps using the above rules:
    
    Template:     NNCB
    After step 1: NCNBCHB
    After step 2: NBCCNBBBCBHCB
    After step 3: NBBBCNCCNBBNBNBBCHBHHBCHB
    After step 4: NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB
    This polymer grows quickly. After step 5, it has length 97; After step 10, it has length 3073. After step 10, B occurs 1749 times, C occurs 298 times, H occurs 161 times, and N occurs 865 times; taking the quantity of the most common element (B, 1749) and subtracting the quantity of the least common element (H, 161) produces 1749 - 161 = 1588.
    
    Apply 10 steps of pair insertion to the polymer template and find the most and least common elements in the result. What do you get if you take the quantity of the most common element and subtract the quantity of the least common element?
    
    --- Part Two ---
    The resulting polymer isn't nearly strong enough to reinforce the submarine. You'll need to run more steps of the pair insertion process; a total of 40 steps should do it.
    
    In the above example, the most common element is B (occurring 2192039569602 times) and the least common element is H (occurring 3849876073 times); subtracting these produces 2188189693529.
    
    Apply 40 steps of pair insertion to the polymer template and find the most and least common elements in the result. What do you get if you take the quantity of the most common element and subtract the quantity of the least common element?

---
+++ 2021/14/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace aoc
+{
+    record InsertionRule
+    {
+        public string Needle { get; set; }
+        public string Insert { get; set; }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Report.Start();
+            var (template, insertionRules) = LoadInput("input.txt");
+            // (template, insertionRules) = LoadInput("sample.txt");
+            
+            Polimerize(insertionRules, template, 10).AsResult1();
+            Polimerize(insertionRules, template, 40).AsResult2();
+
+            Report.End();
+        }
+
+        private static long Polimerize(List<InsertionRule> rules, string template, int steps)
+        {
+            var newPairs = CountPairs(template);
+
+            for (int i = 0; i < steps; i++)
+            {
+                var oldPairs = new Dictionary<string, long>(newPairs);
+                foreach (var old in rules)
+                {
+                    var newLeft = old.Needle.First() + old.Insert;
+                    var newRight = old.Insert + old.Needle.Last();
+
+                    var count = oldPairs.TryGetDef(old.Needle, 0L);
+                    newPairs.Change(old.Needle, (c) => c - count);
+                    newPairs.Change(newLeft,  (c) => c + count);
+                    newPairs.Change(newRight, (c) => c + count);
+                }
+            }
+
+            var occurences = CountLetters(newPairs);
+
+            var (_, leastCount) = occurences.First().Debug("least");
+            var (_, mostCount) = occurences.Last().Debug("most");
+
+            return mostCount - leastCount;
+        }
+
+        private static Dictionary<string, long> CountPairs(string template)
+        {
+            var newPairs = new Dictionary<string, long>
+            {
+                ["_" + template[0]] = 1L, // left edge
+                [template[^1] + "_"] = 1L // right edge
+            };
+            for (int i = 0; i < template.Length - 1; i++)
+            {
+                var pair = $"{template[i]}{template[i + 1]}";
+                var count = newPairs.TryGetDef(pair, 0L);
+                newPairs[pair] = count + 1;
+            }
+
+            return newPairs;
+        }
+
+        private static List<(char letter, long Sum)> CountLetters(Dictionary<string, long> newPairs)
+        {
+            var occurences = newPairs
+                .SelectMany(SplitPairCount)
+                .Where(l => l.letter != '_')
+                .GroupBy(key => key.letter, val => val.count, 
+                    (letter, counts) => (letter, Sum: counts.Sum()/2))
+                .OrderBy(l => l.Sum)
+                .Peek()
+                .ToList();
+            return occurences;
+        }
+
+        private static IEnumerable<(char letter, long count)> SplitPairCount(KeyValuePair<string, long> pair)
+        {
+            return pair.Key.Select(c => (c, pair.Value));
+        }
+
+        public static (string template, List<InsertionRule> insertions) LoadInput(string inputTxt)
+        {
+            var parts = File
+                .ReadAllLines(inputTxt)
+                .Select(s => s.Trim())
+                .GroupByLineSeperator()
+                .ToList();
+
+            string template = parts.First().First();
+
+            var insertions = parts.Last()
+                .Select(s => s.ParseRegex(@"^(.+) -> (.+)$", 
+                    m => new InsertionRule() {
+                        Needle = m.Groups[1].Value,
+                        Insert = m.Groups[2].Value,
+                    }))
+                .ToList();
+
+            return (template, insertions);
+        }
+    }
+}
