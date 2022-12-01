cee4295d973f622db897136d6cf364320bfaff52
Lukas Bugaj <lukas.bugaj@cas.de>
2022-12-01 22:53:10 +0100

    --- Day 1: Calorie Counting ---
    Santa's reindeer typically eat regular reindeer food, but they need a lot of magical energy to deliver presents on Christmas. For that, their favorite snack is a special type of star fruit that only grows deep in the jungle. The Elves have brought you on their annual expedition to the grove where the fruit grows.
    
    To supply enough magical energy, the expedition needs to retrieve a minimum of fifty stars by December 25th. Although the Elves assure you that the grove has plenty of fruit, you decide to grab any fruit you see along the way, just in case.
    
    Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
    
    The jungle must be too overgrown and difficult to navigate in vehicles or access from the air; the Elves' expedition traditionally goes on foot. As your boats approach land, the Elves begin taking inventory of their supplies. One important consideration is food - in particular, the number of Calories each Elf is carrying (your puzzle input).
    
    Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
    
    --- Part Two ---
    By the time you calculate the answer to the Elves' question, they've already realized that the Elf carrying the most Calories of food might eventually run out of snacks.
    
    To avoid this unacceptable situation, the Elves would instead like to know the total Calories carried by the top three Elves carrying the most Calories. That way, even if one of those Elves runs out of snacks, they still have two backups.
    
    Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?

---
+++ 2022/01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace aoc
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Report.Start();
+
+      var elfs = LoadElfs("input.txt")
+        .Select((elf, index) =>
+        new {
+          No = index + 1,
+          TotalCalories = elf.Select(item => long.Parse(item)).Sum(),
+        })
+        .ToList();
+
+      var richestElf = elfs
+        .OrderByDescending(elf => elf.TotalCalories)
+        .First()
+        .AsResult1();
+
+      var top3 = elfs
+        .OrderByDescending(elf => elf.TotalCalories)
+        .Take(3)
+        .Sum(elf => elf.TotalCalories)
+        .AsResult2();
+
+      Report.End();
+    }
+
+    public static List<List<string>> LoadElfs(string inputTxt)
+    {
+      return File
+        .ReadAllLines(inputTxt)
+        .Select(s => s.Trim())
+        .GroupByLineSeperator()
+        .ToList();
+    }
+  }
+}
