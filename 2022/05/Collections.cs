1114f445f348b3a1465f6d87dae8f352856b1f2e
Lukas Bugaj <lukas.bugaj@cas.de>
2022-12-05 20:57:28 +0100

    --- Day 5: Supply Stacks ---
    The expedition can depart as soon as the final supplies have been unloaded from the ships. Supplies are stored in stacks of marked crates, but because the needed supplies are buried under many other crates, the crates need to be rearranged.
    
    The ship has a giant cargo crane capable of moving crates between stacks. To ensure none of the crates get crushed or fall over, the crane operator will rearrange them in a series of carefully-planned steps. After the crates are rearranged, the desired crates will be at the top of each stack.
    
    After the rearrangement procedure completes, what crate ends up on top of each stack?
    
    --- Part Two ---
    As you watch the crane operator expertly rearrange the crates, you notice the process isn't following your prediction.
    
    Some mud was covering the writing on the side of the crane, and you quickly wipe it away. The crane isn't a CrateMover 9000 - it's a CrateMover 9001.
    
    The CrateMover 9001 is notable for many new and exciting features: air conditioning, leather seats, an extra cup holder, and the ability to pick up and move multiple crates at once.
    
    Before the rearrangement process finishes, update your simulation so that the Elves know where they should stand to be ready to unload the final supplies. After the rearrangement procedure completes, what crate ends up on top of each stack?

---
+++ 2022/05/Collections.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace aoc
+{
+    public static class EnumberableCollectionsExtensions
+    {
+        public static long MultiplyAll(this IEnumerable<int> numbers)
+        {
+            return numbers.Select(n => (long)n).MultiplyAll();
+        }
+        public static long MultiplyAll(this IEnumerable<long> numbers)
+        {
+            return numbers.Aggregate((a, b) => a * b);
+        }
+        public static long MultiplyMany<T>(this IEnumerable<T> items, Func<T, long> longSelector)
+        {
+            return items.Select(longSelector).MultiplyAll();
+        }
+
+        public static IEnumerable<T> IntersectMany<T>(this IEnumerable<IEnumerable<T>> enumberable)
+        {
+            var items = enumberable.ToList();
+            var allItems = enumberable.SelectMany(i => i).ToList();
+
+            return items.Aggregate(allItems, (intersect, next) => intersect.Intersect(next).ToList());
+        }
+        public static IEnumerable<T> UnionMany<T>(this IEnumerable<IEnumerable<T>> enumberable)
+        {
+            return enumberable.Aggregate(new List<T>(), (union, next) => union.Union(next).ToList());
+        }
+
+        public static IEnumerable<IEnumerable<T>> Chunk<T>(this IEnumerable<T> source, int chunkSize)
+        {
+            if (chunkSize == 0)
+                throw new ArgumentNullException();
+
+            var enumer = source.GetEnumerator();
+            while (enumer.MoveNext())
+            {
+                yield return Take(enumer.Current, enumer, chunkSize);
+            }
+        }
+
+        private static IEnumerable<T> Take<T>(T head, IEnumerator<T> tail, int chunkSize)
+        {
+            while (true)
+            {
+                yield return head;
+                if (--chunkSize == 0)
+                    break;
+                if (tail.MoveNext())
+                    head = tail.Current;
+                else
+                    break;
+            }
+        }
+
+        public static void ForEach<T>(this IEnumerable<T> items, Action<T> action){
+            foreach (var item in items)
+            {
+                action(item);
+            }
+        }
+
+        public static IEnumerable<T> WhereNth<T>(this IEnumerable<T> items, int nStep){
+            return items.Where((x, i) => i % nStep == 0);
+        }
+
+        public static IEnumerable<R> WhereNth<T, R>(this IEnumerable<T> items, int nStep, Func<T,int,R> resultSelector){
+            
+            return items
+                .Select((x, i) => new Tuple<T,int>(x, i))
+                .Where(t => t.Item2 % nStep == 0)
+                .Select(t => resultSelector(t.Item1, t.Item2));
+        }
+    }
+}
---
+++ 2022/05/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace aoc
+{
+    record Rearrangement
+    {
+        public int Amount { get; set; }
+        public int From { get; set; }
+
+        public int Dest { get; set; }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Report.Start();
+            var inputParts = LoadInputParts("input.txt");
+            //inputParts = LoadInputParts("sample.txt");
+
+            SolvePart1(inputParts);
+            SolvePart2(inputParts);
+
+            Report.End();
+        }
+
+        private static void SolvePart1(List<List<string>> inputParts)
+        {
+            var stacks = LoadStartingStacks(inputParts[0]);
+            var rearrangements = LoadRearrangements(inputParts[1]);
+
+            foreach (var move in rearrangements)
+            {
+                var fromStack = stacks[move.From - 1];
+                var destStack = stacks[move.Dest - 1];
+                foreach (var _ in Enumerable.Range(1, move.Amount))
+                {
+                    destStack.Push(fromStack.Pop());
+                }
+            }
+            
+            stacks.Select(s => s.Pop()).ToCommaString("").AsResult1();
+        }
+
+        
+        private static void SolvePart2(List<List<string>> inputParts)
+        {
+            var stacks = LoadStartingStacks(inputParts[0]);
+            var rearrangements = LoadRearrangements(inputParts[1]);
+
+            foreach (var move in rearrangements)
+            {
+                var fromStack = stacks[move.From - 1];
+                var destStack = stacks[move.Dest - 1];
+                var tmp = new Stack<string>();
+                foreach (var _ in Enumerable.Range(1, move.Amount))
+                {
+                    tmp.Push(fromStack.Pop());
+                }
+                foreach (var item in tmp)
+                {
+                    destStack.Push(item);
+                }
+            }
+
+            stacks.Select(s => s.Pop()).ToCommaString("").AsResult2();
+        }
+        private static List<Stack<string>> LoadStartingStacks(List<string> stackDefinitions)
+        {
+            var reversedDefs = stackDefinitions.Select(x => x).Reverse().ToList();
+            var stacks = reversedDefs
+                .First()
+                .Splizz(" ")
+                .Select(_ => new Stack<string>())
+                .ToList();
+
+            foreach (var def in reversedDefs.Skip(1))
+            {
+                def
+                    .Select(character => character.ToString())
+                    .Skip(1)
+                    .WhereNth(4, (s, i) => new { CargoItem = s, CharIndex = i})
+                    .Where(x => !string.IsNullOrWhiteSpace(x.CargoItem))
+                    .ForEach(x => stacks[x.CharIndex / 4].Push(x.CargoItem));
+            }
+
+            return stacks;
+        }
+
+        private static List<Rearrangement> LoadRearrangements(List<string> inputPart)
+        {
+            return inputPart
+               .Select(s => s.ParseRegex(@"^move (\d+) from (\d+) to (\d+)$", m => new Rearrangement()
+               {
+                   Amount = int.Parse(m.Groups[1].Value),
+                   From = int.Parse(m.Groups[2].Value),
+                   Dest = int.Parse(m.Groups[3].Value),
+               })).ToList();
+        }
+
+        public static List<List<string>> LoadInputParts(string inputTxt)
+        {
+            return File
+                .ReadAllLines(inputTxt)
+                .GroupByLineSeperator()
+                .ToList();
+        }
+    }
+}
