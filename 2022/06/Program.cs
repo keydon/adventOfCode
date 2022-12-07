5295c407b27ea1a004ead42f0a46b9bf062bbe97
Lukas Bugaj <lukas.bugaj@cas.de>
2022-12-07 05:58:06 +0100

    --- Day 6: Tuning Trouble --- The preparations are finally complete; you and the Elves leave camp on foot and begin to make your way toward the star fruit grove.
    As you move through the dense undergrowth, one of the Elves gives you a handheld device. He says that it has many fancy features, but the most important one to set up right now is the communication system.
    
    However, because he's heard you have significant experience dealing with signal-based systems, he convinced the other Elves that it would be okay to give you their one malfunctioning device - surely you'll have no problem fixing it.
    
    How many characters need to be processed before the first start-of-packet marker is detected?
    
    --- Part Two ---
    Your device's communication system is correctly detecting packets, but still isn't working. It looks like it also needs to look for messages.
    
    How many characters need to be processed before the first start-of-message marker is detected?

---
+++ 2022/06/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace aoc
+{
+    record Foo<TPoint> : IHasPosition<TPoint>
+        where TPoint : IPointish
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public string A { get; set; }
+        public string B { get; set; }
+
+        public TPoint Pos { get; set; }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Report.Start();
+            var packets = LoadSignal("input.txt").ToList();
+            //packets = LoadSignal("sample.txt");
+
+            DetectMarker(packets, 4).AsResult1();
+            
+            DetectMarker(packets, 14).AsResult2();
+
+            Report.End();
+        }
+
+        private static int DetectMarker(List<string> packets, int sequenceLength)
+        {
+            for (int i = (sequenceLength-1); i < packets.Count; i++)
+            {
+                var sequence = Enumerable.Range(0, sequenceLength)
+                    .Select(d => packets[i - d])
+                    .ToHashSet();
+                if (sequence.Count == sequenceLength)
+                {
+                    return i + 1;
+                }
+            }
+            throw new Exception("Marker not found");
+        }
+
+        public static List<string> LoadSignal(string inputTxt)
+        {
+            return File
+                .ReadAllLines(inputTxt)
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .Select(s => s.Trim())
+                .SelectMany(c => c.ToCharArray().Select(a => a.ToString()))
+                .ToList();
+        }
+    }
+}
