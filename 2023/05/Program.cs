2dc8127b5d7f8f29dd86c559eb4deb799e24693c
Lukas Bugaj <lukas.bugaj@cas.de>
2023-12-05 00:44:55 +0100

    WIP Day 5

---
+++ 2023/05/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace aoc
+{
+    record Foo<TPoint> : IHasPosition<TPoint>
+        where TPoint : IPointish
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public string A { get; set; }
+        public string B { get; set; }
+
+        public TPoint Pos { get; set; }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Report.Start();
+            var foos = LoadFoos("input.txt");
+            //foos = LoadFoos("sample.txt");
+
+
+            Report.End();
+        }
+
+        public static void LoadFoos(string inputTxt)
+        {
+            var foos = File
+                .ReadAllLines(inputTxt)
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .Select(s => s.Trim())
+
+             //.GroupByLineSeperator()
+             //.Parse2DMap((p, t) => new Foo<Point2> { Pos = p, A = t })
+             //.SelectMany(r => r.Splizz(",", ";"))
+             //.Where(a => a.foo == '#')
+             //.Select(int.Parse)
+             //.Select(long.Parse)  
+             //  .Select(s => s.ParseRegex(@"^mem\[(\d+)\] = (\d+)$", m => new Foo<Point2>()
+             //  {
+             //      X = int.Parse(m.Groups[1].Value),
+             //      Y = int.Parse(m.Groups[2].Value),
+             //      A = m.Groups[1].Value,
+             //      B = m.Groups[2].Value,
+             //  }))
+             //.Where(f = f)
+             //.ToDictionary(
+             //    (a) => new Vector3(a.x, a.y),
+             //    (a) => new Foo(new Vector3(a.x, a.y))
+             //);
+             //.ToArray()
+             .ToList()
+            ;
+
+            return foos;
+        }
+    }
+}
